b301a6a69967f6b1bd117deab1aa97b1ed68230d
alex.buratta <fulminealex03@gmail.com>
2019-11-02 12:02:27 +0100

    misteri

---
+++ Misteri/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Misteri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = Mistero.Mistero1(10, 7);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
